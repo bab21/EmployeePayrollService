cea6161f4b02f9dff385d051a2e35a7ee425a89f
bab21 <2016061@iiitdmj.ac.in>
2020-10-12 19:48:32 +0530

    UC1:reading and writing employee payroll data using console

--- src/main/java/com/capgemini/employeepayrollservice/EmployeePayrollService.java
+++ src/main/java/com/capgemini/employeepayrollservice/EmployeePayrollService.java
@@ -1,7 +1,31 @@
 package com.capgemini.employeepayrollservice;
+import java.util.*;
 
 public class EmployeePayrollService {
+	private List<EmployeePayrollData> employeePayrollList;
+	
+	public EmployeePayrollService(List<EmployeePayrollData> employeePayrollList) {
+		this.employeePayrollList=employeePayrollList;
+	}
+	
 	public static void main(String[] args) {
-		System.out.println("Inside main");
+		List<EmployeePayrollData> employeePayrollList=new ArrayList<EmployeePayrollData>();
+		EmployeePayrollService employeePayrollService=new EmployeePayrollService(employeePayrollList);
+		Scanner consoleInputReader=new Scanner(System.in);
+		employeePayrollService.readEmployeePayrollData(consoleInputReader);
+		employeePayrollService.writeEmployeePayrollData();
+	}
+	private void readEmployeePayrollData(Scanner consoleInputReader) {
+		System.out.println("Enter Employee ID: ");
+		int id=consoleInputReader.nextInt();
+		System.out.println("Enter Employee Name: ");
+		String name=consoleInputReader.next();
+		System.out.println("Enter Employee Salary: ");
+		double salary=consoleInputReader.nextDouble();
+		employeePayrollList.add(new EmployeePayrollData(id,name,salary));
+		
+	}
+	private void writeEmployeePayrollData() {
+		System.out.println("Employee Payroll Data"+employeePayrollList);
 	}
 }
